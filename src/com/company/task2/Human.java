2ea150c0b5a909f126cab374221886d8398bf057
Maks <luckymaks15@mail.ru>
2020-08-31 20:36:40 +0300

    Edet name

--- src/com/company/task2/Human.java
+++ src/com/company/task2/Human.java
@@ -1,18 +1,18 @@
 package com.company.task2;
 
 public class Human {
-    public String name;
+    private String name;
 
-    public int age;
-    public double height;
-    public double weight;
+    private int age;
+    private double height;
+    private double weight;
 
     public Human (String name,int age,double height, double weight ){
         this.name = name;
         this.age = age;
         this.height = height;
         this.weight = weight;
-       System.out.print();
+
 
     }
     public void say (){
