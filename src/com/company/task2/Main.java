d162af3d3b976bd94f088696c3cd1a889c7dd55a
Maks <luckymaks15@mail.ru>
2020-08-31 20:18:54 +0300

    Edet name

--- src/com/company/task2/Main.java
+++ src/com/company/task2/Main.java
@@ -3,8 +3,8 @@ package com.company.task2;
 public class Main {
 
     public static void main(String[] args) {
-        Human num = new  Human("Dima", 12, 1.75, 65.8);
-      num.say();
+        Human human = new  Human("Dima", 12, 1.75, 65.8);
+        human.say();
 
 
     }
