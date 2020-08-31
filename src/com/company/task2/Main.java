8255a32d093ffdff4f4645e0746fa00eec3d5210
Maks <luckymaks15@mail.ru>
2020-08-31 20:03:47 +0300

    First commit

---
+++ src/com/company/task2/Main.java
@@ -0,0 +1,11 @@
+package com.company.task2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Human num = new  Human("Dima", 12, 1.75, 65.8);
+      num.say();
+
+
+    }
+}
