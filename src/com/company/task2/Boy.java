ea65efbd0b456275ed8f8bb90f6023453792944a
Maks <luckymaks15@mail.ru>
2020-09-01 11:54:04 +0300

    first progress

--- src/com/company/task2/Boy.java
+++ src/com/company/task2/Boy.java
@@ -1,4 +1,16 @@
 package com.company.task2;
 
-public class Boy {
+public class Boy extends Human {
+
+    public Boy(String name, int age, double height, double weight) {
+        super(name, age, height, weight);
+    }
+
+    @Override
+    public void say() {
+        super.say();
+    }
 }
+
+
+
--- src/com/company/task2/Main.java
+++ src/com/company/task2/Main.java
@@ -3,8 +3,12 @@ package com.company.task2;
 public class Main {
 
     public static void main(String[] args) {
-        Human gir1 = new Girl("Anna",19,1.79,60);
+       Human gir1 = new Girl("Anna",19,1.79,60);
        gir1.say();
 
+
+       Human boy1= new Boy("Jack",20,1.85,75);
+       boy1.say();
+       // Как то так ?
     }
 }
