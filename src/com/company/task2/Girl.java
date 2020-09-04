cc4a7a654219126c8b42317d1ccc581b264c0c9e
Maks <luckymaks15@mail.ru>
2020-09-04 19:53:49 +0300

    first progress

--- src/com/company/task2/Girl.java
+++ src/com/company/task2/Girl.java
@@ -1,14 +1,16 @@
 package com.company.task2;
 
 public class Girl extends Human {
-
-    public Girl(String name, int age, double height, double weight) {
+    private Hobby hobby;
+    public Girl(String name, int age, double height, double weight, Hobby hobby) {
         super(name, age, height, weight);
+        this.hobby= hobby;
     }
 
     @Override
     public void say() {
         super.say();
     }
+
 
 }
---
+++ src/com/company/task2/Hobby.java
@@ -0,0 +1,18 @@
+package com.company.task2;
+
+public class Hobby {
+    private String name;
+    private int years;
+    private  String description;
+
+
+    public Hobby(String name, int years,String description){
+        this.name = name;
+        this.years= years;
+        this.description = description;
+    }
+
+    public void info (){
+        System.out.println("Hobby:" + name + "years:" + years+ "description:"+ description);
+    }
+}
