67f6d052797069983bc52de1e68c48148543ab49
Maks <luckymaks15@mail.ru>
2020-09-06 13:34:05 +0300

    new progress

--- src/com/company/task2/Friend.java
+++ src/com/company/task2/Friend.java
@@ -1,4 +1,17 @@
 package com.company.task2;
 
 public class Friend {
+    private String name;
+    private int years;
+    private  String say;
+
+    public Friend (String name, int years,String say){
+        this.name = name;
+        this.years= years;
+        this.say = say;
+    }
+    public void info (){
+        System.out.println("Hobby:" + name + " years: " + years + " say:"+ say);
+    }
+
 }
--- src/com/company/task2/Girl.java
+++ src/com/company/task2/Girl.java
@@ -1,10 +1,12 @@
 package com.company.task2;
 
 public class Girl extends Human {
+    private Friend friend;
     private Hobby hobby;
-    public Girl(String name, int age, double height, double weight, Hobby hobby) {
+    public Girl(String name, int age, double height, double weight, Hobby hobby,Friend friend) {
         super(name, age, height, weight);
         this.hobby= hobby;
+        this.friend = friend;
     }
     public void getInfo(){
         hobby.info();
