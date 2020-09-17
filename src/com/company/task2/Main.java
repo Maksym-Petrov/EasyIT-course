41b1773ff5fd8c59c73c9c3167360d7d59e06a5c
Maks <luckymaks15@mail.ru>
2020-09-17 11:51:22 +0300

    First

--- src/com/company/task2/Main.java
+++ src/com/company/task2/Main.java
@@ -1,24 +1,27 @@
 package com.company.task2;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-         Girl girl = new Girl("Anna",19,1.79,60,
-                 new Hobby(" football", 2," The game where players kick the ball"),
-                 new Friend("Harry",13,"I am friend"));
-          girl.say();
-        girl.getInfo();
-         girl.getInf();
+        HashMap<String,String> Telephonebook = new HashMap<>();
 
+   Telephonebook.put("SARA", "099658265");
+        Telephonebook.put("ANNA", "06787348730");
+       Telephonebook.put("LARA", "0988645730");
+       Telephonebook.put("HARRY", "0950967548730");
+       Telephonebook.put("JONI", "08765748730");
 
-        Human boy= new Boy("Jack",20,1.85,75,
-                new Hobby(" basketball", 2," The game where players kick the ball"),
-                new Friend(" lena",  13, " I am friend") );
-        boy.say();
-        boy.getInfo();
-        boy.getInf();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Name: ");
+        String name=sc.next().toUpperCase();
 
+        if(Telephonebook.containsKey(name)) {
+            System.out.println(name +": "+Telephonebook.get(name));
+        }
 
 
     }
